0c68f071d526a6c79f6031b4ae0b1c65d9d3340c
Michael Richardson <mcr@sandelman.ca>
2021-07-10 18:23:50 -0400

    updates to include mbedtls

--- example1.c
+++ example1.c
@@ -7,16 +7,52 @@
 #include <netdb.h>
 #include <unistd.h>
 
+#include "mbedtls/net.h"
+#include "mbedtls/ssl.h"
+#include "mbedtls/entropy.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/debug.h"
+
 #define SERVER_PORT 443
 #define SERVER_NAME "howsmyssl.com"
 #define GET_REQUEST "GET /a/check HTTP/1.0\r\n\r\n"
 
+
+static void my_debug( void *ctx, int level, const char *file, int line, const char *str ) {
+
+  ((void) level);
+  fprintf( (FILE *) ctx, "%s:%04d: %s", file, line, str );
+  fflush( (FILE *) ctx );
+}
+
+
 int main( void )
 {
-    int ret, len, server_fd;
+    int ret, len;
     unsigned char buf[1024];
     struct sockaddr_in server_addr;
     struct hostent *server_host;
+
+    mbedtls_net_context server_fd;
+    mbedtls_entropy_context entropy;
+    mbedtls_ctr_drbg_context ctr_drbg;
+    mbedtls_ssl_context ssl;
+    mbedtls_ssl_config conf;
+
+    mbedtls_net_init( &server_fd );
+    mbedtls_ssl_init( &ssl );
+    mbedtls_ssl_config_init( &conf );
+    mbedtls_x509_crt_init( &cacert );
+    mbedtls_ctr_drbg_init( &ctr_drbg );
+
+    mbedtls_entropy_init( &entropy );
+    if( ( ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy,
+                                       (const unsigned char *) pers,
+                                       strlen( pers ) ) ) != 0 )
+      {
+        printf( " failed\n  ! mbedtls_ctr_drbg_seed returned %d\n", ret );
+        goto exit;
+      }
 
     /*
      * Start the connection
@@ -25,33 +61,33 @@ int main( void )
                                                  SERVER_PORT );
     fflush( stdout );
 
-    if( ( server_host = gethostbyname( SERVER_NAME ) ) == NULL )
-    {
-        printf( " failed\n  ! gethostbyname failed\n\n");
+    if( ( ret = mbedtls_net_connect( &server_fd, SERVER_NAME,
+                                     SERVER_PORT, MBEDTLS_NET_PROTO_TCP ) ) != 0 )
+      {
+        printf( " failed\n  ! mbedtls_net_connect returned %d\n\n", ret );
         goto exit;
-    }
-
-    if( ( server_fd = socket( AF_INET, SOCK_STREAM, IPPROTO_IP) ) < 0 )
-    {
-        printf( " failed\n  ! socket returned %d\n\n", server_fd );
-        goto exit;
-    }
-
-    memcpy( (void *) &server_addr.sin_addr,
-            (void *) server_host->h_addr,
-                     server_host->h_length );
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons( SERVER_PORT );
-
-    if( ( ret = connect( server_fd, (struct sockaddr *) &server_addr,
-                         sizeof( server_addr ) ) ) < 0 )
-    {
-        printf( " failed\n  ! connect returned %d\n\n", ret );
-        goto exit;
-    }
+      }
 
     printf( " ok\n" );
+
+    if( ( ret = mbedtls_ssl_config_defaults( &conf, MBEDTLS_SSL_IS_CLIENT,
+                                             MBEDTLS_SSL_TRANSPORT_STREAM,
+                                             MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 ) {
+      mbedtls_printf( " failed\n ! mbedtls_ssl_config_defaults returned %d\n\n", ret ); goto exit;
+    }
+
+    mbedtls_ssl_conf_authmode( &conf, MBEDTLS_SSL_VERIFY_NONE );
+
+    mbedtls_ssl_conf_rng( &conf, mbedtls_ctr_drbg_random, &ctr_drbg );
+    mbedtls_ssl_conf_dbg( &conf, my_debug, stdout );
+
+    if( ( ret = mbedtls_ssl_set_hostname( &ssl, "Mbed TLS Server 1" ) ) != 0 ) {
+      mbedtls_printf( " failed\n ! mbedtls_ssl_set_hostname returned %d\n\n", ret );
+      goto exit;
+    }
+
+    mbedtls_ssl_set_bio( &ssl, &server_fd, mbedtls_net_send, mbedtls_net_recv, NULL );
+
 
     /*
      * Write the GET request
@@ -61,7 +97,7 @@ int main( void )
 
     len = sprintf( (char *) buf, GET_REQUEST );
 
-    while( ( ret = write( server_fd, buf, len ) ) <= 0 )
+    while( ( ret = mbedtls_ssl_write( &ssl, buf, len ) ) <= 0 )
     {
         if( ret != 0 )
         {
@@ -82,7 +118,7 @@ int main( void )
     {
         len = sizeof( buf ) - 1;
         memset( buf, 0, sizeof( buf ) );
-        ret = read( server_fd, buf, len );
+        ret = mbedtls_ssl_read( &ssl, buf, len );
 
         if( ret <= 0 )
         {
@@ -97,7 +133,11 @@ int main( void )
 
 exit:
 
-    close( server_fd );
+    mbedtls_net_free( &server_fd );
+    mbedtls_ssl_free( &ssl );
+    mbedtls_ssl_config_free( &conf );
+    mbedtls_ctr_drbg_free( &ctr_drbg );
+    mbedtls_entropy_free( &entropy );
 
 #ifdef WIN32
     printf( "  + Press Enter to exit this program.\n" );
